aba69dacb9704bb858aa7e8418a4c98e613bccf0
Gilad Halevy <gilad.halevy@walmartlabs.com>
2020-01-16 22:27:43 -0800

    adding hello world. Also this is my first commit

--- guessthenumber.py
+++ guessthenumber.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
 
-magic_number = 5
+import random
 
-guess = input("I have chosen a number between 1-10, please try to guess it: ")
+magic_number = random.randint(1,21)
 
-if int(guess) == magic_number:
-    print("You guessed it!")
-else:
-    print("Sorry, you did not guess correctly!")
+guess = 500
+
+while not int(guess) == magic_number:
+
+    guess = input("I have chosen a number between 1-20, please try to guess it: ")
+
+    if int(guess) == magic_number:
+        print("You guessed it!")
+    else:
+        print("Sorry, you did not guess correctly!")
