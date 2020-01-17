003d4937e7cc1b2bf5f1c380a42a31a8e0fe55bd
Gilad Halevy <gilad.halevy@walmartlabs.com>
2020-01-16 22:03:37 -0800

    adding hello world. Also this is my first commit

--- guessthenumber.py
+++ guessthenumber.py
@@ -5,6 +5,6 @@ magic_number = 5
 guess = raw_input("I have chosen a number between 1-10, please try to guess it: ")
 
 if guess == magic_number:
-    print "You guessed it!"
+    print("You guessed it!")
 else
-    print "Sorry, you did not guess correctly!"
+    print("Sorry, you did not guess correctly!")
