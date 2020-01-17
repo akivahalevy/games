2f541d34a8c2ee86c4320b0961e7b3aade6dc1f4
Gilad Halevy <ghalevy@gmail.com>
2020-01-16 22:00:54 -0800

    number game

---
+++ guessthenumber.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+magic_number = 5
+
+guess = raw_input("I have chosen a number between 1-10, please try to guess it: ")
+
+if guess == magic_number:
+    print "You guessed it!"
+else
+    print "Sorry, you did not guess correctly!"
